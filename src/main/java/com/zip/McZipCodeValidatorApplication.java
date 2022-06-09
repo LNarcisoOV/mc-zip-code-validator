0347da28934979adc9fdbd635e15699f1ce466e2
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-06-09 10:54:06 +0000

    First commit.

---
+++ src/main/java/com/zip/McZipCodeValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.zip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McZipCodeValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McZipCodeValidatorApplication.class, args);
+	}
+
+}
