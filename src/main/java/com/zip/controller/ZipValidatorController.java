af64376e27af3ecfaee07248c1f26fa6481bc005
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-07-22 09:59:33 +0000

    Add log.

--- src/main/java/com/zip/controller/ZipValidatorController.java
+++ src/main/java/com/zip/controller/ZipValidatorController.java
@@ -1,5 +1,7 @@
 package com.zip.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,8 @@ import com.zip.service.ZipValidatorService;
 @RestController
 @RequestMapping("/zip")
 public class ZipValidatorController {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(ZipValidatorController.class);
 
     @Autowired
     private ZipValidatorService zipValidatorService;
@@ -24,9 +28,11 @@ public class ZipValidatorController {
     public ResponseEntity<Boolean> zipValidator(@PathVariable("zipCode") String zipCode) {
 
         try {
+            LOGGER.info("Validatin ZIP code: {} ", zipCode);
             Boolean isValidZipCode = zipValidatorService.isValid(zipCode);
             return new ResponseEntity<>(isValidZipCode, HttpStatus.OK);
         } catch (RuntimeException runtimeException) {
+            LOGGER.error("Error to validate zip code ", zipCode);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
